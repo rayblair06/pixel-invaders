1977b1afe581b1c49e64b61b8426c6024bbc52ed
Ray Blair <rayblair06@hotmail.com>
2025-05-05 18:42:30 +0100

    Comments

--- main.c
+++ main.c
@@ -7,25 +7,32 @@ const int PLAYER_SPEED = 5;
 
 int main(int argc, char *argv[])
 {
+    // Initialize SDL
     SDL_Init(SDL_INIT_VIDEO);
 
+    // Create a window
     SDL_Window *window = SDL_CreateWindow("Space Wars",
                                           SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                                           SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
+    // Create a renderer
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
     bool running = true;
 
+    // Main game loop
     SDL_Event event;
 
+    // Initialize player position
     SDL_Rect player = {SCREEN_WIDTH / 2 - 25, SCREEN_HEIGHT - 60, 50, 20};
 
     // keep track of key states
     const Uint8 *keystate = SDL_GetKeyboardState(NULL);
 
+    // Main loop
     while (running)
     {
+        // Handle events
         while (SDL_PollEvent(&event))
         {
             if (event.type == SDL_QUIT)
@@ -65,6 +72,7 @@ int main(int argc, char *argv[])
         SDL_Delay(16); // ~60 FPS
     }
 
+    // Clean up
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
