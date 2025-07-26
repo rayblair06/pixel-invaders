49b6f039af075c744a031ab68ed01409514d8efe
Ray Blair <rayblair06@hotmail.com>
2025-07-26 21:18:21 +0100

    Go big or go home

--- constants.c
+++ constants.c
@@ -3,9 +3,9 @@
 /**
  * Global Contants
  */
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
+// const int SCREEN_WIDTH = 800;
+// const int SCREEN_HEIGHT = 600;
 
 // Debating this size
-// const int SCREEN_WIDTH = 1024;
-// const int SCREEN_HEIGHT = 768;
+const int SCREEN_WIDTH = 1024;
+const int SCREEN_HEIGHT = 768;
