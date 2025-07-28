b25758ef771dda4085994cf10e539bf24cb33795
Ray Blair <rayblair06@hotmail.com>
2025-07-28 20:22:26 +0100

    Revert "Missing windows libs"
    This reverts commit c8ad65f95c0d9f8bb606f8816a68cc203105aa8b.

--- boss.c
+++ boss.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "audio.h"
 #include "boss.h"
 #include "constants.h"
--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_mixer.h>
