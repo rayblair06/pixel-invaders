c77922b8c82ae68f1cd9a0d9b03a984c3d746af8
Ray Blair <rayblair06@hotmail.com>
2025-07-28 19:59:26 +0100

    Updated compile steps

--- cmake/FindSDL2_All.cmake
+++ cmake/FindSDL2_All.cmake
@@ -3,33 +3,61 @@ set(SDL2_ALL_LIBS "")
 set(SDL2_ALL_INCLUDE_DIRS "")
 
 if(WIN32)
+    # Windows: Download prebuilt SDL2 packages
     include(${CMAKE_SOURCE_DIR}/cmake/SetupSDL2.cmake)
     link_directories(${SDL2_ALL_LIB_DIRS})
     set(SDL2_ALL_LIBS SDL2 SDL2main SDL2_image SDL2_ttf SDL2_mixer)
     set(SDL2_ALL_FOUND TRUE)
 
 elseif(APPLE)
+    # macOS: Use pkg-config for headers, absolute paths for libs
     find_package(PkgConfig REQUIRED)
     pkg_check_modules(SDL2 REQUIRED sdl2)
     pkg_check_modules(SDL2_IMAGE REQUIRED SDL2_image)
     pkg_check_modules(SDL2_TTF REQUIRED SDL2_ttf)
     pkg_check_modules(SDL2_MIXER REQUIRED SDL2_mixer)
 
+    # Header include directories
     set(SDL2_ALL_INCLUDE_DIRS
         ${SDL2_INCLUDE_DIRS}
         ${SDL2_IMAGE_INCLUDE_DIRS}
         ${SDL2_TTF_INCLUDE_DIRS}
         ${SDL2_MIXER_INCLUDE_DIRS}
     )
-    set(SDL2_ALL_LIBS
-        ${SDL2_LIBRARIES}
-        ${SDL2_IMAGE_LIBRARIES}
-        ${SDL2_TTF_LIBRARIES}
-        ${SDL2_MIXER_LIBRARIES}
+
+    # Resolve full library paths using Homebrew
+    execute_process(
+        COMMAND brew --prefix sdl2
+        OUTPUT_VARIABLE SDL2_PREFIX
+        OUTPUT_STRIP_TRAILING_WHITESPACE
     )
+    execute_process(
+        COMMAND brew --prefix sdl2_image
+        OUTPUT_VARIABLE SDL2_IMAGE_PREFIX
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    execute_process(
+        COMMAND brew --prefix sdl2_ttf
+        OUTPUT_VARIABLE SDL2_TTF_PREFIX
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    execute_process(
+        COMMAND brew --prefix sdl2_mixer
+        OUTPUT_VARIABLE SDL2_MIXER_PREFIX
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+    set(SDL2_ALL_LIBS
+        ${SDL2_PREFIX}/lib/libSDL2.dylib
+        ${SDL2_IMAGE_PREFIX}/lib/libSDL2_image.dylib
+        ${SDL2_TTF_PREFIX}/lib/libSDL2_ttf.dylib
+        ${SDL2_MIXER_PREFIX}/lib/libSDL2_mixer.dylib
+    )
+
     set(SDL2_ALL_FOUND TRUE)
 
 else() # Linux
+    # Linux: Use pkg-config (libs are in default linker paths)
     find_package(PkgConfig REQUIRED)
     pkg_check_modules(SDL2 REQUIRED sdl2)
     pkg_check_modules(SDL2_IMAGE REQUIRED SDL2_image)
@@ -47,7 +75,7 @@ else() # Linux
         ${SDL2_IMAGE_LIBRARIES}
         ${SDL2_TTF_LIBRARIES}
         ${SDL2_MIXER_LIBRARIES}
-        m # math library for Linux
+        m # math library for sqrtf, etc.
     )
     set(SDL2_ALL_FOUND TRUE)
 endif()
@@ -55,7 +83,3 @@ endif()
 if(NOT SDL2_ALL_FOUND)
     message(FATAL_ERROR "SDL2 libraries not found!")
 endif()
-
-# Expose vars
-set(SDL2_ALL_INCLUDE_DIRS ${SDL2_ALL_INCLUDE_DIRS} PARENT_SCOPE)
-set(SDL2_ALL_LIBS ${SDL2_ALL_LIBS} PARENT_SCOPE)
