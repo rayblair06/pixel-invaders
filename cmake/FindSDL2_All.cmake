9f846d465b763e30690104fd434c47f7ea85e445
Ray Blair <rayblair06@hotmail.com>
2025-07-28 20:45:12 +0100

    Revert "Updated compile steps"
    This reverts commit 0397edcc123ea79ed06aeec8f629fc40ed741bfd.

--- cmake/FindSDL2_All.cmake
+++ cmake/FindSDL2_All.cmake
@@ -2,8 +2,7 @@ set(SDL2_ALL_FOUND FALSE)
 set(SDL2_ALL_LIBS "")
 set(SDL2_ALL_INCLUDE_DIRS "")
 
-if(WIN32)
-    # Windows: Use prebuilt libraries
+if (WIN32)
     include(${CMAKE_SOURCE_DIR}/cmake/SetupSDL2.cmake)
 
     set(SDL2_ALL_LIBS
@@ -24,13 +23,14 @@ if(WIN32)
     set(SDL2_ALL_FOUND TRUE)
 
 elseif(APPLE)
-    # macOS: Use Homebrew
+    # macOS: Use pkg-config for headers, absolute paths for libs
     find_package(PkgConfig REQUIRED)
     pkg_check_modules(SDL2 REQUIRED sdl2)
     pkg_check_modules(SDL2_IMAGE REQUIRED SDL2_image)
     pkg_check_modules(SDL2_TTF REQUIRED SDL2_ttf)
     pkg_check_modules(SDL2_MIXER REQUIRED SDL2_mixer)
 
+    # Header include directories
     set(SDL2_ALL_INCLUDE_DIRS
         ${SDL2_INCLUDE_DIRS}
         ${SDL2_IMAGE_INCLUDE_DIRS}
@@ -38,10 +38,27 @@ elseif(APPLE)
         ${SDL2_MIXER_INCLUDE_DIRS}
     )
 
-    execute_process(COMMAND brew --prefix sdl2 OUTPUT_VARIABLE SDL2_PREFIX OUTPUT_STRIP_TRAILING_WHITESPACE)
-    execute_process(COMMAND brew --prefix sdl2_image OUTPUT_VARIABLE SDL2_IMAGE_PREFIX OUTPUT_STRIP_TRAILING_WHITESPACE)
-    execute_process(COMMAND brew --prefix sdl2_ttf OUTPUT_VARIABLE SDL2_TTF_PREFIX OUTPUT_STRIP_TRAILING_WHITESPACE)
-    execute_process(COMMAND brew --prefix sdl2_mixer OUTPUT_VARIABLE SDL2_MIXER_PREFIX OUTPUT_STRIP_TRAILING_WHITESPACE)
+    # Resolve full library paths using Homebrew
+    execute_process(
+        COMMAND brew --prefix sdl2
+        OUTPUT_VARIABLE SDL2_PREFIX
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    execute_process(
+        COMMAND brew --prefix sdl2_image
+        OUTPUT_VARIABLE SDL2_IMAGE_PREFIX
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    execute_process(
+        COMMAND brew --prefix sdl2_ttf
+        OUTPUT_VARIABLE SDL2_TTF_PREFIX
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    execute_process(
+        COMMAND brew --prefix sdl2_mixer
+        OUTPUT_VARIABLE SDL2_MIXER_PREFIX
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
 
     set(SDL2_ALL_LIBS
         ${SDL2_PREFIX}/lib/libSDL2.dylib
@@ -49,10 +66,11 @@ elseif(APPLE)
         ${SDL2_TTF_PREFIX}/lib/libSDL2_ttf.dylib
         ${SDL2_MIXER_PREFIX}/lib/libSDL2_mixer.dylib
     )
+
     set(SDL2_ALL_FOUND TRUE)
 
 else() # Linux
-    # Linux: Use pkg-config
+    # Linux: Use pkg-config (libs are in default linker paths)
     find_package(PkgConfig REQUIRED)
     pkg_check_modules(SDL2 REQUIRED sdl2)
     pkg_check_modules(SDL2_IMAGE REQUIRED SDL2_image)
@@ -65,13 +83,12 @@ else() # Linux
         ${SDL2_TTF_INCLUDE_DIRS}
         ${SDL2_MIXER_INCLUDE_DIRS}
     )
-
     set(SDL2_ALL_LIBS
         ${SDL2_LIBRARIES}
         ${SDL2_IMAGE_LIBRARIES}
         ${SDL2_TTF_LIBRARIES}
         ${SDL2_MIXER_LIBRARIES}
-        m
+        m # math library for sqrtf, etc.
     )
     set(SDL2_ALL_FOUND TRUE)
 endif()
